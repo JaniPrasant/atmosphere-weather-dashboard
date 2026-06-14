93a0d84aa4980602dadeab0deccb871c423e1bfa
Jani Prashant <jani.prasant2810@gmail.com>
2026-06-14 12:39:48 +0530

    fix catch-all path routing syntax for compatibility matching

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, '../frontend/dist')));
-  app.get('*', (req, res) => {
+  app.get('(.*)', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
   });
 }
